88b89a1dea3524262e9428579bdda32f9ef51e98
ckaotik <ckaotik@gmail.com>
2016-07-26 23:20:00 +0200

    Removed config addon

--- Broker_Garbage-Config/Broker_Garbage-Config.lua
+++
@@ -1,9 +0,0 @@
-local _, BGC = ...
-Broker_Garbage_Config = BGC	-- allow external access
-
-
-LibStub("tekKonfig-AboutPanel").new("Broker_Garbage", "Broker_Garbage")
-
-if InterfaceOptionsFrame:IsVisible() then
-	InterfaceOptionsFrame_OpenToCategory("Broker_Garbage")
-end
