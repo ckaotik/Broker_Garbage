cad9bf56ee1fa4360197f9fe5d621215c3d64cf5
ckaotik <ckaotik@gmail.com>
2016-08-01 00:26:51 +0200

    Use module name for vendor options panel

--- Broker_Garbage/vendor.lua
+++ Broker_Garbage/vendor.lua
@@ -185,10 +185,9 @@ function plugin:OnEnable()
 
 	self:RegisterEvent('MERCHANT_SHOW')
 
-	local optionsTable = LibStub('LibOptionsGenerate-1.0'):GetOptionsTable(self.db)
-	      optionsTable.name = addonName .. ' - Vendor'
+	local optionsTable = LibStub('LibOptionsGenerate-1.0'):GetOptionsTable(self.db, nil, addon.configLocale)
+	      optionsTable.name = addonName .. ' - ' .. self:GetName()
 	LibStub('AceConfig-3.0'):RegisterOptionsTable(self.name, optionsTable)
-	-- LibStub('AceConfigDialog-3.0'):AddToBlizOptions(self.name, 'Vendor', 'Broker_Garbage') --, 'general')
 end
 
 function plugin:MERCHANT_SHOW()
