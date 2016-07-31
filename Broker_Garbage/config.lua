0510aa05f49456c9a42bbf5ba8ab0f6fd0eb56e1
ckaotik <ckaotik@gmail.com>
2016-08-01 00:17:39 +0200

    Updated config, moved slash commands into separate file

--- Broker_Garbage/config.lua
+++ Broker_Garbage/config.lua
@@ -1,24 +1,43 @@
 local addonName, addon, _ = ...
 
--- GLOBALS: _G, LibStub, C_Timer
--- GLOBALS: INTERFACEOPTIONS_ADDONCATEGORIES, InterfaceOptionsFrame_OpenToCategory, GetItemInfo
--- GLOBALS: ipairs, type, tonumber, select
-local tremove = table.remove
-local format = string.format
+-- GLOBALS: _G, LibStub
+-- GLOBALS: InterfaceAddOnsList_Update, InterfaceOptionsList_DisplayPanel, GetItemInfo, GetCoinTextureString
+-- GLOBALS: ipairs, type, select, setmetatable, math, tostring
 
--- Blizzard configuration panel
-local function OpenConfiguration(self, args)
-	-- remove placeholder configuration panel
-	for i, panel in ipairs(_G.INTERFACEOPTIONS_ADDONCATEGORIES) do
-		if panel == self then
-			tremove(INTERFACEOPTIONS_ADDONCATEGORIES, i)
-			break
+local AceConfigDialog = LibStub('AceConfigDialog-3.0')
+
+local function GetConfigurationVariables()
+	local itemQualities = {}
+	itemQualities[-1] = '- Do not sell -' -- TODO localize
+	for quality, color in pairs(_G.ITEM_QUALITY_COLORS) do
+		if quality >= 0 then
+			itemQualities[quality] = color.hex .. _G['ITEM_QUALITY'..quality..'_DESC'] .. '|r'
 		end
 	end
-	self:SetScript('OnShow', nil)
-	self:Hide()
 
-	-- initialize panel
+	local function GetDescriptionWidget(key, option)
+		local value = math.abs(option)
+		return {
+			type = 'description',
+			fontSize = 'medium',
+			name = ('%s: %s'):format(
+				addon.configLocale[key] or key,
+				key:find('money') and GetCoinTextureString(value) or value
+			),
+		}
+	end
+	local function GetItemListLabels(key, value)
+		local label = tostring(value)
+		if type(key) == 'number' then
+			local itemName, itemLink, _, _, _, _, _, _, _, icon, _ = GetItemInfo(key)
+			label = ('|T%s:0|t %s'):format(icon or 'Interface\\Icons\\INV_Misc_QuestionMark', itemLink or key)
+		else
+			-- TODO this needs fixing, table.sort dies on string + number values
+			return key, nil
+		end
+		return key, label
+	end
+
 	local types = {
 		disableKey = {
 			NONE  = _G['NONE_KEY'],
@@ -31,256 +50,73 @@ local function OpenConfiguration(self, args)
 			icon = addon.FormatMoney(540321, 'icon'),
 			dot  = addon.FormatMoney(540321, 'dot'),
 		},
+		disenchantSkillOffset = 'unsigned',
+		sellUnusableQuality = itemQualities,
+		sellOutdatedQuality = itemQualities,
+		-- keep = 'multiselect',
+		-- toss = 'multiselect',
+		moneyEarned = GetDescriptionWidget,
+		moneyLost   = GetDescriptionWidget,
+		numDeleted  = GetDescriptionWidget,
+		numSold     = GetDescriptionWidget,
 	}
-	for _, setting in ipairs({'version', 'prices', 'keep', 'toss', 'moneyEarned', 'moneyLost', 'numDeleted', 'numSold'}) do
+
+	-- Hide various settings ... for now.
+	for _, setting in ipairs({
+		'version',
+		'dataSources',
+		'prices',
+		'keep',
+		'toss',
+	}) do
 		types[setting] = '*none*'
 	end
 
+	local locale = setmetatable({}, {
+		__index = addon.configLocale,
+	})
+	locale.keepValues = GetItemListLabels
+	locale.tossValues = GetItemListLabels
+
+	-- variable, typeMappings, L, includeNamespaces, callback
+	return addon.db, types, locale, false, nil
+end
+
+-- Blizzard configuration panel
+local function InitializeConfiguration(self, args)
+	local AceConfig = LibStub('AceConfig-3.0')
 	LibStub('LibDualSpec-1.0'):EnhanceDatabase(addon.db, addonName)
-	local AceConfig, AceConfigDialog = LibStub('AceConfig-3.0'), LibStub('AceConfigDialog-3.0')
-	local optionsTable = LibStub('LibOptionsGenerate-1.0'):GetOptionsTable(addon.db, types, nil, false)
+
+	-- Initialize main panel.
+	local optionsTable = LibStub('LibOptionsGenerate-1.0'):GetOptionsTable(GetConfigurationVariables())
 	      optionsTable.name = addonName
 	AceConfig:RegisterOptionsTable(addonName, optionsTable)
-	local panel = AceConfigDialog:AddToBlizOptions(addonName, nil, nil)
 
-	-- add entries for submodules
+	-- Add panels for submodules.
 	local AceConfigRegistry = LibStub('AceConfigRegistry-3.0')
 	for name, subModule in addon:IterateModules() do
 		if AceConfigRegistry.tables[subModule.name] then
 			AceConfigDialog:AddToBlizOptions(subModule.name, name, addonName)
 		end
-	end --]]
-	local profileOptions = LibStub('AceDBOptions-3.0'):GetOptionsTable(addon.db)
-	      profileOptions.name = addonName .. ' - ' .. profileOptions.name
-	AceConfig:RegisterOptionsTable(addonName..'_profiles', profileOptions)
-	AceConfigDialog:AddToBlizOptions(addonName..'_profiles', 'Profiles', addonName)
+	end
 
-	-- do all this only once. next time, only show this panel
-	OpenConfiguration = function(panel, args)
-		InterfaceOptionsFrame_OpenToCategory(addonName)
+	if addon.db.defaults and addon.db.defaults.profile and next(addon.db.defaults.profile) then
+		-- Add panel for profile settings.
+		local profileOptions = LibStub('AceDBOptions-3.0'):GetOptionsTable(addon.db)
+		      profileOptions.name = addonName .. ' - ' .. profileOptions.name
+		AceConfig:RegisterOptionsTable(addonName..'_profiles', profileOptions)
+		AceConfigDialog:AddToBlizOptions(addonName..'_profiles', 'Profiles', addonName)
 	end
-	if not panel:IsVisible() then
-		C_Timer.After(0.05, OpenConfiguration)
-	end
+
+	-- Restore original OnShow handler.
+	self:SetScript('OnShow', self.origOnShow)
+	self.origOnShow = nil
+
+	InterfaceAddOnsList_Update()
+	InterfaceOptionsList_DisplayPanel(self)
 end
 
--- create a fake configuration panel
-local panel = CreateFrame('Frame')
-      panel.name = addonName
-      panel:Hide()
-      panel:SetScript('OnShow', OpenConfiguration)
-InterfaceOptions_AddCategory(panel)
-
--- slash commands
-local L = {
-	['invalidArgument'] = 'You supplied an invalid argument. Please check your input and try again.',
-	['slashCommandHelp'] = [[supports |cffee6622/garbage|r, |cffee6622/garb|r, |cffee6622/junk|r with these commands:
-    |cFF36BFA8config|r opens the options panel.
-    |cFF36BFA8add|r |cFF2bff58<list>|r |cFF2bff58<item>|r Add an item/category to a list.
-    |cFF36BFA8remove|r |cFF2bff58<list>|r |cFF2bff58<item>|r Remove item/category from a given list.
-        Possible list names: |cFF2bff58keep|r, |cFF2bff58junk|r, |cFF2bff58vendor|r, |cFF2bff58forceprice|r
-    |cFF36BFA8update|r |cFF2bff58<itemID>|r Refresh saved data
-    |cFF36BFA8format|r |cFF2bff58<text>|r lets you customize the LDB display text, |cFF2bff58reset|r resets it.
-    |cFF36BFA8categories|r |cFF2bff58<item>|r list of used categories with this item.]],
-}
-
-local function GetListName(list)
-	if list == 'keep' or list == 'exclude' or list == 'treasure' then
-		return 'keep'
-	elseif list == 'toss' or list == 'junk' or list =='include' or list == 'garbage' then
-		return 'toss'
-	elseif list == 'sell' or list == 'autosell' or list == 'vendor' or list == 'autoselllist' then
-		return 'toss', 1
-	elseif list == 'vendorprice' or list == 'forceprice' or list == 'forcevendorprice' then
-		return 'prices'
-	end
-end
-
-local COMMAND_ALIAS = { -- map of alias => command
-	options		= 'config',
-	option		= 'config',
-	menu		= 'config',
-	display 	= 'format',
-	glimit 		= 'globallimit',
-	numlines 	= 'tooltiplines',
-	height 		= 'tooltipheight',
-	category 	= 'categories',
-	list 		= 'categories',
-	lists 		= 'categories',
-	updatecache = 'cache',
-	resetcache 	= 'cache',
-	update 		= 'cache',
-}
-
--- + = documented, implemented; - = not documented, implemented
-local COMMAND_PARAMS = {
-	-- + open config UI
-	config = function(param)
-		InterfaceOptionsFrame_OpenToCategory(addonName)
-	end,
-
-	-- + change LDB display format
-	format = function(param)
-		if param == "reset" then
-			addon:SetOption("label", true, addon.defaults.global.label)
-		else
-			addon:SetOption("label", true, param)
-		end
-		addon:ScanInventory()
-	end,
-
-	-- + set local limit for item
-	limit = function(param)
-		local itemID, count = param:match("^[^0-9]-([0-9]+).-([0-9]+)$")
-		itemID 	= tonumber(itemID) or -1
-		count 	= tonumber(count) or -1
-
-		if itemID < 1 or count < 0 then
-			return false
-		end
-
-		addon.itemsCache[itemID] = nil
-
-		local table = addon:SetOption("include", false)
-		table[itemID] = count
-
-		local itemLink = select(2, GetItemInfo(itemID)) or 'BGC.locale.unknown'
-		addon.Print(format('BGC.locale.limitSet', itemLink, count))
-		-- BGC:ListOptionsUpdate("include")
-	end,
-
-	-- + set global limit for item
-	globallimit = function(param)
-		local itemID, count = param:match("^[^0-9]-([0-9]+).-([0-9]+)$")
-		itemID 	= tonumber(itemID) or -1
-		count 	= tonumber(count) or -1
-
-		if itemID < 1 or count < 0 then
-			return false
-		end
-
-		addon.itemsCache[itemID] = nil
-
-		local table = addon:GetOption("include", true)
-		table[itemID] = count
-
-		local itemLink = select(2, GetItemInfo(itemID)) or 'BGC.locale.unknown'
-		addon.Print(format('BGC.locale.limitSet', itemLink, count))
-		-- BGC:ListOptionsUpdate("include")
-	end,
-
-	-- + set number of displayed LDB tooltip lines
-	tooltiplines = function(param)
-		param = tonumber(param)
-		if not param then
-			return false
-		end
-		addon:SetOption("tooltipNumItems", true, param)
-		addon:ScanInventory()
-		-- TODO: update config panel
-	end,
-
-	-- + set pixel height of LDB tooltip
-	tooltipheight = function(param)
-		param = tonumber(param)
-		if not param then
-			return false
-		end
-		addon:SetOption("tooltipMaxHeight", true, param)
-		-- TODO: update config panel
-	end,
-
-	-- + list all listed categories an item belongs to
-	categories = function(param)
-		param = addon.GetItemID(param) or param
-		if not param then
-			return false
-		end
-
-		local itemLink = select(2, GetItemInfo(param))
-		-- TODO: FIXME
-		local result = addon.GetItemListCategories(addon.GetCached(param))
-		if not result or #result < 1 then
-			addon.Print(format("%s is in no used category.", itemLink))
-		else
-			for _, listName in ipairs(result) do
-				addon.Print(format("%s is in category %s.", itemLink, listName))
-			end
-		end
-	end,
-
-	-- + add an <itemID/itemLink/category/...> to a <list>
-	add = function(param)
-		-- TODO: allow limit count?
-		local list, item = param:match("^(%S*)%s*(.-)%s*$")
-		local list, listValue = GetListName(list and list:lower() or '')
-		if not list or not item then
-			return false
-		end
-		local itemID = tonumber(item) or addon.GetItemID(item) or item
-		addon.Add(list, itemID, listValue)
-	end,
-	remove = function(param)
-		local list, item = param:match("^(%S*)%s*(.-)%s*$")
-		      list = GetListName(list and list:lower() or '')
-		if not list or not item then
-			return false
-		end
-		local itemID = tonumber(item) or addon.GetItemID(item) or item
-		addon.Remove(list, itemID)
-	end,
-
-	cache = function(item)
-		local item = item and tonumber(item) or item
-		if item and type(item) == "number" then
-			addon.UpdateAllCaches(item)
-		else
-			addon.UpdateAllDynamicItems()
-		end
-	end
-}
-
-function addon:RegisterSlashCommand(command, handler, aliases, helpText)
-	if COMMAND_PARAMS[command] then
-		return false, 'Command is already registered.'
-	end
-
-	if type(aliases) == 'table' then
-		for _, alias in ipairs(aliases) do
-			COMMAND_ALIAS[alias] = command
-		end
-	elseif aliases then
-		COMMAND_ALIAS[aliases] = command
-	end
-	COMMAND_PARAMS[command] = handler
-
-	if helpText and helpText ~= '' then
-		L['slashCommandHelp'] = L['slashCommandHelp'] .. '\n    |cFF36BFA8' .. command .. '|r ' .. helpText
-	end
-
-	return true
-end
-
-local function handler(msg)
-	local command, param = msg:match("^(%S*)%s*(.-)%s*$")
-	command = command:lower()
-
-	local baseCommand = COMMAND_ALIAS[command] or command
-	if COMMAND_PARAMS[baseCommand] then
-		-- command can be handled
-		if COMMAND_PARAMS[baseCommand](param) == false then
-			addon.Print(L['invalidArgument'])
-		end
-		return
-	end
-
-	-- commands don't match
-	addon.Print(L['slashCommandHelp'])
-end
-
-local slashName = addonName:upper()
-_G['SLASH_' .. addonName .. '1'] = "/garbage"
-_G['SLASH_' .. addonName .. '2'] = "/garb"
-_G['SLASH_' .. addonName .. '3'] = "/junk"
-
-SlashCmdList[addonName] = handler
+-- Create a placeholder configuration panel.
+local panel = AceConfigDialog:AddToBlizOptions(addonName)
+panel.origOnShow = panel:GetScript('OnShow')
+panel:SetScript('OnShow', InitializeConfiguration)
---
+++ Broker_Garbage/slashcmd.lua
@@ -0,0 +1,218 @@
+local addonName, addon, _ = ...
+
+-- GLOBALS: _G, LibStub
+-- GLOBALS: InterfaceAddOnsList_Update, InterfaceOptionsList_DisplayPanel, InterfaceOptionsFrame_OpenToCategory, GetItemInfo
+-- GLOBALS: ipairs, type, tonumber, select
+
+local L = {
+	['invalidArgument'] = 'You supplied an invalid argument. Please check your input and try again.',
+	['slashCommandHelp'] = [[supports |cffee6622/garbage|r, |cffee6622/garb|r, |cffee6622/junk|r with these commands:
+    |cFF36BFA8config|r opens the options panel.
+    |cFF36BFA8add|r |cFF2bff58<list>|r |cFF2bff58<item>|r Add an item/category to a list.
+    |cFF36BFA8remove|r |cFF2bff58<list>|r |cFF2bff58<item>|r Remove item/category from a given list.
+        Possible list names: |cFF2bff58keep|r, |cFF2bff58junk|r, |cFF2bff58vendor|r, |cFF2bff58forceprice|r
+    |cFF36BFA8update|r |cFF2bff58<itemID>|r Refresh saved data
+    |cFF36BFA8format|r |cFF2bff58<text>|r lets you customize the LDB display text, |cFF2bff58reset|r resets it.
+    |cFF36BFA8categories|r |cFF2bff58<item>|r list of used categories with this item.]],
+}
+
+local function GetListName(list)
+	if list == 'keep' or list == 'exclude' or list == 'treasure' then
+		return 'keep'
+	elseif list == 'toss' or list == 'junk' or list =='include' or list == 'garbage' then
+		return 'toss'
+	elseif list == 'sell' or list == 'autosell' or list == 'vendor' or list == 'autoselllist' then
+		return 'toss', 1
+	elseif list == 'vendorprice' or list == 'forceprice' or list == 'forcevendorprice' then
+		return 'prices'
+	end
+end
+
+local COMMAND_ALIAS = { -- map of alias => command
+	options		= 'config',
+	option		= 'config',
+	menu		= 'config',
+	display 	= 'format',
+	glimit 		= 'globallimit',
+	numlines 	= 'tooltiplines',
+	height 		= 'tooltipheight',
+	category 	= 'categories',
+	list 		= 'categories',
+	lists 		= 'categories',
+	updatecache = 'cache',
+	resetcache 	= 'cache',
+	update 		= 'cache',
+}
+
+-- + = documented, implemented; - = not documented, implemented
+local COMMAND_PARAMS = {
+	-- + open config UI
+	config = function(param)
+		InterfaceOptionsFrame_OpenToCategory(addonName)
+	end,
+
+	-- + change LDB display format
+	format = function(param)
+		if param == "reset" then
+			addon:SetOption("label", true, addon.db.defaults.global.label)
+		else
+			addon:SetOption("label", true, param)
+		end
+		addon:ScanInventory()
+	end,
+
+	-- + set local limit for item
+	limit = function(param)
+		local itemID, count = param:match("^[^0-9]-([0-9]+).-([0-9]+)$")
+		itemID 	= tonumber(itemID) or -1
+		count 	= tonumber(count) or -1
+
+		if itemID < 1 or count < 0 then
+			return false
+		end
+
+		addon.itemsCache[itemID] = nil
+
+		local table = addon:SetOption("include", false)
+		table[itemID] = count
+
+		local itemLink = select(2, GetItemInfo(itemID)) or _G.UNKNOWN
+		addon.Print(("%s has been assigned a limit of %d."):format(itemLink, count))
+		-- BGC:ListOptionsUpdate("include")
+	end,
+
+	-- + set global limit for item
+	globallimit = function(param)
+		local itemID, count = param:match("^[^0-9]-([0-9]+).-([0-9]+)$")
+		itemID 	= tonumber(itemID) or -1
+		count 	= tonumber(count) or -1
+
+		if itemID < 1 or count < 0 then
+			return false
+		end
+
+		addon.itemsCache[itemID] = nil
+
+		local table = addon:GetOption("include", true)
+		table[itemID] = count
+
+		local itemLink = select(2, GetItemInfo(itemID)) or _G.UNKNOWN
+		addon.Print(("%s has been assigned a limit of %d."):format(itemLink, count))
+		-- BGC:ListOptionsUpdate("include")
+	end,
+
+	-- + set number of displayed LDB tooltip lines
+	tooltiplines = function(param)
+		param = tonumber(param)
+		if not param then
+			return false
+		end
+		addon:SetOption("tooltipNumItems", true, param)
+		addon:ScanInventory()
+		-- TODO: update config panel
+	end,
+
+	-- + set pixel height of LDB tooltip
+	tooltipheight = function(param)
+		param = tonumber(param)
+		if not param then
+			return false
+		end
+		addon:SetOption("tooltipMaxHeight", true, param)
+		-- TODO: update config panel
+	end,
+
+	-- + list all listed categories an item belongs to
+	categories = function(param)
+		param = addon.GetItemID(param) or param
+		if not param then
+			return false
+		end
+
+		local itemLink = select(2, GetItemInfo(param))
+		-- TODO: FIXME
+		local result = addon.GetItemListCategories(addon.GetCached(param))
+		if not result or #result < 1 then
+			addon.Print(("%s is in no used category."):format(itemLink))
+		else
+			for _, listName in ipairs(result) do
+				addon.Print(("%s is in category %s."):format(itemLink, listName))
+			end
+		end
+	end,
+
+	-- + add an <itemID/itemLink/category/...> to a <list>
+	add = function(param)
+		-- TODO: allow limit count?
+		local list, item = param:match("^(%S*)%s*(.-)%s*$")
+		local list, listValue = GetListName(list and list:lower() or '')
+		if not list or not item then
+			return false
+		end
+		local itemID = tonumber(item) or addon.GetItemID(item) or item
+		addon.Add(list, itemID, listValue)
+	end,
+	remove = function(param)
+		local list, item = param:match("^(%S*)%s*(.-)%s*$")
+		      list = GetListName(list and list:lower() or '')
+		if not list or not item then
+			return false
+		end
+		local itemID = tonumber(item) or addon.GetItemID(item) or item
+		addon.Remove(list, itemID)
+	end,
+
+	cache = function(item)
+		local item = item and tonumber(item) or item
+		if item and type(item) == "number" then
+			addon.UpdateAllCaches(item)
+		else
+			addon.UpdateAllDynamicItems()
+		end
+	end
+}
+
+function addon:RegisterSlashCommand(command, handler, aliases, helpText)
+	if COMMAND_PARAMS[command] then
+		return false, 'Command is already registered.'
+	end
+
+	if type(aliases) == 'table' then
+		for _, alias in ipairs(aliases) do
+			COMMAND_ALIAS[alias] = command
+		end
+	elseif aliases then
+		COMMAND_ALIAS[aliases] = command
+	end
+	COMMAND_PARAMS[command] = handler
+
+	if helpText and helpText ~= '' then
+		L['slashCommandHelp'] = L['slashCommandHelp'] .. '\n    |cFF36BFA8' .. command .. '|r ' .. helpText
+	end
+
+	return true
+end
+
+local function handler(msg)
+	local command, param = msg:match("^(%S*)%s*(.-)%s*$")
+	command = command:lower()
+
+	local baseCommand = COMMAND_ALIAS[command] or command
+	if COMMAND_PARAMS[baseCommand] then
+		-- command can be handled
+		if COMMAND_PARAMS[baseCommand](param) == false then
+			addon.Print(L['invalidArgument'])
+		end
+		return
+	end
+
+	-- commands don't match
+	addon.Print(L['slashCommandHelp'])
+end
+
+local slashName = addonName:upper()
+_G['SLASH_' .. addonName .. '1'] = "/garbage"
+_G['SLASH_' .. addonName .. '2'] = "/garb"
+_G['SLASH_' .. addonName .. '3'] = "/junk"
+
+SlashCmdList[addonName] = handler
